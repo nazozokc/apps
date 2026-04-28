03e2b4b669e5d4a2b6cbacc174cf609f663d584b
nazozokc <nazozokc@gmail.com>
2026-04-28 22:45:41 +0900

    edit

--- eot/index.ts
+++ eot/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello via Bun!");
+import { readdir } from "fs/promises";
+import { Command } from "commander";
+import { consola } from "consola";
+
+const runCLI = () => {
+  const program = new Command();
+  program
+    .name("eot")
+    .description("dir view tool")
+    .version("1.0.0")
+    .argument("[dir]")
+    .action(async (dir) => {
+      if (dir === undefined) {
+        const dirname = await readdir(".");
+        for (const dirlist of dirname) {
+          consola.log("list");
+          consola.log("-------------------------------");
+          consola.log(dirlist);
+        }
+      } else {
+        const dirname = await readdir(dir);
+        for (const dirlist of dirname) {
+          consola.log("list");
+          consola.log("-------------------------------");
+          consola.log(dirlist);
+        }
+      }
+    });
+};
+
